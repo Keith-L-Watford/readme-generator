dcefe4b1891156eaea635f49cdc93ba20e94107e
Keith Watford <keith.l.watford@gmail.com>
2021-06-23 19:48:09 -0400

    trying to create an array holding the license info

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,33 @@
-const index = require('./../index');
+// const index = require('./../index');
+const mit = {
+  badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+  text: "",
+}
+
+const unlicense = {
+  badge: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
+  text: "",
+}
+
+const boost = {
+  badge: "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
+  text: "",
+}
+
+const mozilla = {
+  badge: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+  text: "",
+}
+
+const apache = {
+  badge: "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+  text: "",
+}
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  switch (licence) {
-    case "MIT":
-      '[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
-      break;
-  
-    default:
-      "";
-  }
+
 }
 
 // TODO: Create a function that returns the license link
@@ -20,17 +37,7 @@ function renderLicenseLink(license) {}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  switch (whichLicense) {
-    case "MIT":
-    "LICENSE TEST LICENSE"
-        break;
-    // case value:
-
-    //     break;
-
-    default:
-        " ";
-}
+  
 }
 
 // TODO: Create a function to generate markdown for README
