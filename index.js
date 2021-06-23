36a4e9dc4871df38d5213513004a627690649476
Keith Watford <keith.l.watford@gmail.com>
2021-06-23 18:19:51 -0400

    almost done with user inputs, just need to troubleshoot tests

--- index.js
+++ index.js
@@ -7,52 +7,47 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 // const questions = [];
 inquirer
     .prompt([
-        // {
-        //     type: 'input',
-        //     message: 'What would you like the title of your README.md for your app to be?',
-        //     name: 'title',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'Describe your app.',
-        //     name: 'description',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'What are the steps required to install your project?',
-        //     name: 'installation',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'Provide instructions and examples for use.',
-        //     name: 'usage',
-        // },
-        // {
-        //     type: 'list',
-        //     message: 'Which license would you like to use?',
-        //     choices: ['The Unlicense', 'MIT', 'Apache 2.0 License', 'Mozilla Public License 2.0', 'Boost'],
-        //     name: 'license',
-        // },
-        // {
-        //     type: 'input',
-        //     message: '',
-        //     name: 'contributing',
-        // },
+        {
+            type: 'input',
+            message: 'What is the name of your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Describe your project.',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required to install your project?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Provide instructions and examples for use for your project.',
+            name: 'usage',
+        },
+        {
+            type: 'list',
+            message: 'Which license would you like to use for your project?',
+            choices: ['The Unlicense', 'MIT', 'Apache 2.0 License', 'Mozilla Public License 2.0', 'Boost'],
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'Please list any contributing parties.',
+            name: 'contributing',
+        },
         {
             type: 'input',
             message: 'Whats your github username?',
             name: 'github',
         },
-        // {
-        //     type: 'input',
-        //     message: '',
-        //     name: 'questions',
-        // },
-        // {
-        //     type: '',
-        //     message: '',
-        //     name: '',
-        // },
+        {
+            type: 'input',
+            message: 'Write any tests for your application.',
+            name: 'test',
+        },
     ])
 
     // TODO: Create a function to write README file
@@ -60,21 +55,29 @@ inquirer
     .then((answers) => {
         console.log(answers);
 
-let readMeFile = `# ${answers.title} \n
+        let readMeFile = `# ${answers.title} \n
 ## Description \n 
 ${answers.description} \n 
 ## Table of Contents \n
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
 - [Questions](#questions)
+- [License](#license)
+
 \n
 ## Installation
 ${answers.installation} \n
 
 ## Usage
 ${answers.usage} \n
+
+## Contributing
+${answers.contributing} \n
+
+## Tests
+${answers.tests} \n
 
 ## Questions
 If you have any questions about my project, please find me on <a href="https://github.com/${answers.github}/" target="blank">GitHub</a>!
