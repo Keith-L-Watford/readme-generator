2a51f3a0f436b8bd2a73f6c591c5ef66c0a9e82a
Keith Watford <keith.l.watford@gmail.com>
2021-06-24 10:22:43 -0400

    testing how to add the copyright text. figured it out.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application ✅
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input ✅
 // const questions = [];
@@ -66,15 +66,26 @@ inquirer
 
         if (licenseChoice === 'MIT') {
            licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-            // console.log(licenseBadge);
+           licenseText = `
+Copyright
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
         } else if (licenseChoice === 'The Unlicense') {
             licenseBadge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+            licenseText = ""
         } else if (licenseChoice === 'Apache 2.0 License') {
             licenseBadge =  "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+            licenseText = ""
         } else if (licenseChoice === 'Mozilla Public License 2.0') {
             licenseBadge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+            licenseText = ""
         } else if (licenseChoice === 'Boost') {
             licenseBadge = "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+            licenseText = ""
         } 
         
         let readMeFile = `# ${answers.title} \n
@@ -106,7 +117,7 @@ If you have any questions about my project, please send me an  <a href="mailto:$
 Find me on <a href="https://github.com/${answers.github}/" target="blank">GitHub</a> too! \n
 
 ## License
- ${licenseTest} \n
+ ${licenseText} \n
 `;
 
 
